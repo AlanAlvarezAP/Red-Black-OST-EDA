2f7175cc03db9dfe8c0f927cbd7d275f6b9d993a
AlanAlvarezAP <alan.alvarez@ucsp.edu.pe>
2025-10-26 14:54:50 -0500

    Inicio Preprocesamiento

--- Preprocesamiento.h
+++ Preprocesamiento.h
@@ -3,12 +3,19 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <unordered_set>
+#include <fstream>
+#include <regex>
 
 class Preprocesador {
-	std::vector<std::string> stop_words;
+	std::unordered_set<std::string> stop_words;
+	std::string final_text;
+	std::vector<std::string> tokens;
+	std::ifstream lector;
+	std::regex regex;
 public:
-	Preprocesador();
-	void Limpieza(std::string& texto);
-
+	Preprocesador(const std::string& stop);
+	void Limpieza(const std::string& texto);
+	void Tokenizar();
 
 };
--- Red-Black-OST-EDA.cpp
+++ Red-Black-OST-EDA.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "Preprocesamiento.h"
+#include "RB_OST.h"
+
 
 int main()
 {
-    std::cout << "Hola prueba EDA";
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    Preprocesador prepo("stop_words.txt");
+    std::string dir;
+    std::cout << " Bienvenido al proyecto de EDA RB-OST dame el nombre del primer archivo" << "\n";
+    std::cin >> dir;
+    prepo.Limpieza(dir);
+    prepo.Tokenizar();
 }
